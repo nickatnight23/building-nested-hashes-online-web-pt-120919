fa14a545943c811aa9f3870bd59375d320d247ef
Latricia Nickelberry <tricianickelberry@gmail.com>
2019-12-20 16:43:36 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,17 +1,16 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch
-      :matriarch
-      :hero
-      :hero_friends
-      }, 
-    :capulet => {
-      :patriarch
-      :matriarch
-      :heroine
-      :heroine_friends
-      }}
-
-  
-end
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
+ 
